537e7205a52b227161bda909a444920afa9d6d34
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2024-07-21 12:19:14 +0700

    Add files via upload

---
+++ W3 CSE_307/in class/demo-javascript.js
@@ -0,0 +1,20 @@
+// bấm vào chữ abc để hiện thông báo
+function Test() {
+    // alert("Hello guy");
+    
+    // câu này khi click vào nhấn OKay là xóa
+    // if(confirm("Are you sure?")) {
+    //     document.getElementById("test").remove();
+
+    let a = 5;
+    let b = 6;
+    if ( a > b ) 
+        console.log("A > B");
+    else 
+        console.log("B > A");
+
+    }
+}
+
+
+
